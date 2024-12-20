b44f2941f3fee363260c360d28938c0ce8a3f487
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-20 19:18:50 +0000

    include guard にパスを含める

---
+++ .idea/fileTemplates/internal/C Header File.h
@@ -0,0 +1,4 @@
+#[[#ifndef]]# ${DIR_PATH.toUpperCase().replace("/", "_")}_${INCLUDE_GUARD}
+#[[#define]]# ${DIR_PATH.toUpperCase().replace("/", "_")}_${INCLUDE_GUARD}
+
+#[[#endif]]#
---
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,12 @@
+#[[#ifndef]]# ${DIR_PATH.toUpperCase().replace("/", "_")}_${INCLUDE_GUARD}
+#[[#define]]# ${DIR_PATH.toUpperCase().replace("/", "_")}_${INCLUDE_GUARD}
+
+${NAMESPACES_OPEN}
+
+class ${NAME} {
+
+};
+
+${NAMESPACES_CLOSE}
+
+#[[#endif]]#
