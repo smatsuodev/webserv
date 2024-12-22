5e28ccf5d45638e883fae20498a4c3d054631f0d
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-22 15:45:05 +0900

    types::Unit

---
+++ src/lib/utils/unit.hpp
@@ -0,0 +1,11 @@
+#ifndef SRC_LIB_UTILS_UNIT_HPP
+#define SRC_LIB_UTILS_UNIT_HPP
+
+namespace types {
+    struct Unit {};
+}
+
+// ReSharper disable once CppInconsistentNaming
+const types::Unit unit = types::Unit();
+
+#endif
