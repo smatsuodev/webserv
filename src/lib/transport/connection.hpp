fa90a72224d317bfdb01de2eb0c47841fe0ebc76
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-24 07:58:49 +0900

    Connection は bufio::Reader を返す

--- src/lib/transport/connection.hpp
+++ src/lib/transport/connection.hpp
@@ -12,11 +12,12 @@ public:
     ~Connection();
 
     int getFd() const;
-    io::IReader &getReader() const;
+    bufio::Reader &getReader() const;
 
 private:
     AutoFd clientFd_;
-    std::auto_ptr<io::IReader> reader_;
+    std::auto_ptr<io::IReader> fdReader_; // bufio::Reader に渡す IReader & の参照先として必要
+    std::auto_ptr<bufio::Reader> reader_;
 };
 
 #endif
