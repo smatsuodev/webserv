c770b6e0e7eae5d46860322d1a84f5450e30af6e
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-27 20:38:45 +0900

    EchoHandler をやめる

--- src/lib/core/handler/read_request_handler.cpp
+++ src/lib/core/handler/read_request_handler.cpp
@@ -1,6 +1,7 @@
 #include "read_request_handler.hpp"
-#include "echo_handler.hpp"
+#include "write_response_handler.hpp"
 #include "core/action.hpp"
+#include "http/response/response.hpp"
 #include "utils/logger.hpp"
 #include "utils/io/reader.hpp"
 #include "utils/types/try.hpp"
@@ -15,8 +16,14 @@ IEventHandler::InvokeResult ReadRequestHandler::invoke(const Context &ctx) {
 
     LOG_DEBUGF("HTTP request parsed");
 
+    // 今は同じ body をそのまま返す
+    http::Headers headers;
+    headers.insert(std::make_pair("Content-Length", utils::toString(req.getBody().size())));
+    headers.insert(std::make_pair("Content-Type", "text/plain"));
+    const http::Response res(http::kStatusOk, "HTTP/1.1", headers, req.getBody());
+
     std::vector<IAction *> actions;
-    actions.push_back(new RegisterEventHandlerAction(ctx.getConnection().unwrap(), new EchoHandler(req.getBody())));
+    actions.push_back(new RegisterEventHandlerAction(ctx.getConnection().unwrap(), new WriteResponseHandler(res)));
     /**
      * クライアントはこれ以上イベントを起こさない可能性がある
      * 続きの handler が実行されるように、擬似的にイベントを通知する
