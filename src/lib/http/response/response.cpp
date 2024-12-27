2e95b0da79abf524c6f9ff18ac5c4f9939490ef7
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-27 20:38:27 +0900

    Response::toString を直す

--- src/lib/http/response/response.cpp
+++ src/lib/http/response/response.cpp
@@ -26,16 +26,20 @@ const std::string &http::Response::getBody() const {
     return body_;
 }
 
+// NOTE: 結構無理やり
 std::string http::Response::toString() const {
     std::vector<std::string> lines;
 
     // status line
-    lines.push_back(utils::format("%s %d %s", httpVersion_, status_, http::getHttpStatusText(status_)));
+    lines.push_back(utils::format("%s %d %s", httpVersion_.c_str(), status_, http::getHttpStatusText(status_).c_str()));
 
     // field lines
     for (Headers::const_iterator it = headers_.begin(); it != headers_.end(); ++it) {
-        lines.push_back(utils::format("%s: %s", it->first, it->second));
+        lines.push_back(utils::format("%s: %s", it->first.c_str(), it->second.c_str()));
     }
+
+    // 空行
+    lines.push_back("");
 
     // message body
     lines.push_back(body_);
