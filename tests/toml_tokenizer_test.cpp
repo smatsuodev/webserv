6fc6451c295a24ca976557493b8ebd9118694ed5
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-29 05:55:11 +0900

    tokenizer test util

--- tests/toml_tokenizer_test.cpp
+++ tests/toml_tokenizer_test.cpp
@@ -3,228 +3,239 @@
 
 using namespace toml;
 
+void testOk(const std::string &text, const Tokenizer::Tokens &expected) {
+    // TODO: use tokenizer
+    const Tokenizer::TokenizeResult result = Err<std::string>("unknown");
+    ASSERT_TRUE(result.isOk());
+    EXPECT_EQ(result.unwrap(), expected);
+}
+
+void testErr(const std::string &text) {
+    // TODO: use tokenizer
+    const Tokenizer::TokenizeResult result = Err<std::string>("unknown");
+    ASSERT_TRUE(result.isErr());
+}
+
 TEST(TokenizerOk, comment) {
     const auto text = R"(# comment)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, commentAfterValue) {
     const auto text = R"(key = "value" # comment)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, notComment) {
     const auto text = R"(key = "# this is not comment")";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, keyValue) {
     const auto text = R"(key = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, intKey) {
     const auto text = R"(123 = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, quotedKey) {
     const auto text = R"("127.0.0.1" = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
-// "a=b" = 0
 TEST(TokenizerOk, keyContainsEq) {
     const auto text = R"("a=b" = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, unicodeKey) {
     const auto text = R"("ʎǝʞ" = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, singleQuotedKey) {
     const auto text = R"('key' = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 
 TEST(TokenizerOk, quotedQuoteKey) {
     const auto text = R"('"quoted key"' = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 // 有効だが非推奨
 TEST(TokenizerOkDiscouraged, emptyKey) {
     const auto text = R"("" = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 // 有効だが非推奨
 TEST(TokenizerOkDiscouraged, emptyKeySingle) {
     const auto text = R"('' = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, dottedKey) {
     const auto text = R"(k1.k2 = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, dottedQuotedKey) {
     const auto text = R"(k1."k2.k3".k4 = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, spaceAroudDot) {
     const auto text = R"(k1 . k2 = 0)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, notFloat) {
     const auto text = R"(3 . 14 = "pi")";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, plusInteger) {
     const auto text = R"(k = +99)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, Integer) {
     const auto text = R"(k = 42)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, integerZero) {
     const auto text = R"()";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, minusInteger) {
     const auto text = R"(k = -17)";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, emptyArray) {
     const auto text = R"(k = [])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, intArray) {
     const auto text = R"(k = [1, 2])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, stringArray) {
     const auto text = R"(k = ["foo", 'bar'])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 
 TEST(TokenizerOk, nestedArray) {
     const auto text = R"(k = [[1, 2], [3, 4]])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, nestedMixedArray) {
     const auto text = R"(k = [[1, 2], ["a", "b"]])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, mixedArray) {
     const auto text = R"(k = [1, "value", [1], { key = "value" }, ])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
-
 
 TEST(TokenizerOk, justStringArray) {
     const auto text = R"(k1.k2 = ["[1]", "{a.b=0}"])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, table) {
     const auto text = R"([table])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, dottedTable) {
     const auto text = R"([a.b.c])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, dottedSpaceTable) {
     const auto text = R"([ a.b.c ])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, dottedSpaceTable2) {
     const auto text = R"([ a . b . c ])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, dottedSpaceTable3) {
     const auto text = R"([ j . "ʞ" . 'l' ])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, inlineTable) {
     const auto text = R"(k = {x=0, y=1})";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, dottedKeyInInlineTable) {
     const auto text = R"(k = { a.b = 0 })";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, arrayOfTable) {
     const auto text = R"([[array]])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerOk, dottedArrayOfTable) {
     const auto text = R"([[a.b]])";
-    EXPECT_TRUE(false);
+    testOk(text, {});
 }
 
 TEST(TokenizerErr, noValue) {
     const auto text = R"(k = )";
-    EXPECT_TRUE(false);
+    testErr(text);
 }
 
 TEST(TokenizerErr, noKey) {
     const auto text = R"( = 0)";
-    EXPECT_TRUE(false);
+    testErr(text);
 }
 
 TEST(TokenizerErr, multipleEq) {
     const auto text = R"(k == 0)";
-    EXPECT_TRUE(false);
+    testErr(text);
 }
 
 TEST(TokenizerErr, multipleKeyValue) {
     const auto text = R"(k1=0 k2=0)";
-    EXPECT_TRUE(false);
+    testErr(text);
 }
 
 TEST(TokenizerErr, emptyInlineTableComma) {
     const auto text = R"(k = {, })";
-    EXPECT_TRUE(false);
+    testErr(text);
 }
 
 TEST(TokenizerErr, emptyArrayComma) {
     const auto text = R"(k = [, ])";
-    EXPECT_TRUE(false);
+    testErr(text);
 }
 
 TEST(TokenizerErr, inlineTableTrailingComma) {
     const auto text = R"(k = { a=1, })";
-    EXPECT_TRUE(false);
+    testErr(text);
 }
 
 TEST(TokenizerErr, invalidTable) {
     const auto text = R"([[[table]]])";
-    EXPECT_TRUE(false);
+    testErr(text);
 }
