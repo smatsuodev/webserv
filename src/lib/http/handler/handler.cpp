53e4c5cfa3b01ecb5ba9d9111247292124d6b0ec
shmorish <110565242+shmorish@users.noreply.github.com>
2025-01-01 16:41:06 +0900

    Add handler serve

--- src/lib/http/handler/handler.cpp
+++ src/lib/http/handler/handler.cpp
@@ -1,8 +1,23 @@
 #include "handler.hpp"
+#include "utils/logger.hpp"
+#include "http/response/response_builder.hpp"
+#include <fstream>
+#include <sstream>
 
 http::IHandler::~IHandler() {}
 
 Result<http::Response, error::AppError> http::Handler::serve(Request &req) {
-    // TODO: implement me
-    return Err(error::kUnknown);
+    const std::string path = req.getRequestTarget().substr(1);
+    if (path.empty()) {
+        LOG_DEBUGF("invalid request target: %s", req.getRequestTarget().c_str());
+        return Err(error::kUnknown);
+    }
+    std::ifstream ifs(path);
+    if (!ifs.is_open()) {
+        LOG_DEBUGF("failed to open file: %s", path.c_str());
+        return Err(error::kUnknown);
+    }
+    std::stringstream ss;
+    ss << ifs.rdbuf();
+    return Ok(http::ResponseBuilder().text(ss.str()).build());
 }
