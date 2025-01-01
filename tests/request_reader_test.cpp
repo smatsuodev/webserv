a9281e5fd2e37855df962d825dd06336dbd36ffb
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-01-02 08:01:41 +0900

    fix tests

--- tests/request_reader_test.cpp
+++ tests/request_reader_test.cpp
@@ -16,8 +16,8 @@ TEST_F(RequestReaderTest, GetNormal) {
     const std::string request = "GET / HTTP/1.1\r\n"
                                 "Host: example.com\r\n\r\n";
     StringReader reader(request);
-    bufio::Reader bufReader(reader);
-    RequestReader reqReader(bufReader);
+    ReadBuffer readBuf(reader);
+    RequestReader reqReader(readBuf);
 
     const auto result = reqReader.readRequest();
     EXPECT_TRUE(result.isOk());
@@ -36,8 +36,8 @@ TEST_F(RequestReaderTest, PostNormal) {
                                 "\r\n"
                                 "hello";
     StringReader reader(request);
-    bufio::Reader bufReader(reader);
-    RequestReader reqReader(bufReader);
+    ReadBuffer readBuf(reader);
+    RequestReader reqReader(readBuf);
 
     const auto result = reqReader.readRequest();
     EXPECT_TRUE(result.isOk());
@@ -56,8 +56,8 @@ TEST_F(RequestReaderTest, GetWouldBlock) {
                                 "Host: example.com\r\n\r\n";
     StringReader sReader(request);
     WouldBlockReader2 reader(sReader);
-    bufio::Reader bufReader(reader, 4);
-    RequestReader reqReader(bufReader);
+    ReadBuffer readBuf(reader);
+    RequestReader reqReader(readBuf);
 
     // kWouldBlock が返るので、Ok になるまで繰り返す
     auto result = reqReader.readRequest();
@@ -80,55 +80,8 @@ TEST_F(RequestReaderTest, PostWouldBlock) {
                                 "hello";
     StringReader sReader(request);
     WouldBlockReader2 reader(sReader);
-    bufio::Reader bufReader(reader, 4);
-    RequestReader reqReader(bufReader);
-
-    auto result = reqReader.readRequest();
-    while (result.isErr()) {
-        result = reqReader.readRequest();
-    }
-
-    const http::Request expected = http::Request(http::kMethodPost, "/", "HTTP/1.1",
-                                                 {
-                                                     std::make_pair("Content-Length", "5"),
-                                                     std::make_pair("Host", "example.com"),
-                                                 },
-                                                 "hello");
-    EXPECT_EQ(result.unwrap(), expected);
-}
-
-TEST_F(RequestReaderTest, GetWouldBlockWithLargeBuf) {
-    const std::string request = "GET / HTTP/1.1\r\n"
-                                "Host: example.com\r\n\r\n";
-    StringReader sReader(request);
-    WouldBlockReader2 reader(sReader);
-    bufio::Reader bufReader(reader);
-    RequestReader reqReader(bufReader);
-
-    // kWouldBlock が返るので、Ok になるまで繰り返す
-    auto result = reqReader.readRequest();
-    while (result.isErr()) {
-        result = reqReader.readRequest();
-    }
-
-    const http::Request expected = http::Request(http::kMethodGet, "/", "HTTP/1.1",
-                                                 {
-                                                     std::make_pair("Host", "example.com"),
-                                                 });
-    EXPECT_EQ(result.unwrap(), expected);
-}
-
-
-TEST_F(RequestReaderTest, PostWouldBlockWithLargeBuf) {
-    const std::string request = "POST / HTTP/1.1\r\n"
-                                "Content-Length: 5\r\n"
-                                "Host: example.com\r\n"
-                                "\r\n"
-                                "hello";
-    StringReader sReader(request);
-    WouldBlockReader2 reader(sReader);
-    bufio::Reader bufReader(reader);
-    RequestReader reqReader(bufReader);
+    ReadBuffer readBuf(reader);
+    RequestReader reqReader(readBuf);
 
     auto result = reqReader.readRequest();
     while (result.isErr()) {
