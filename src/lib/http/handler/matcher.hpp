4c271176c1bd409a2eb1b0f2506c12146aabb747
shmorish <110565242+shmorish@users.noreply.github.com>
2025-01-03 23:48:04 +0900

    .vscode/settings.json を追加し、ファイル関連付けを設定。matcher.hpp の match メソッドを修正し、テストケースをコメントアウト。

--- src/lib/http/handler/matcher.hpp
+++ src/lib/http/handler/matcher.hpp
@@ -1,10 +1,9 @@
 #ifndef SRC_LIB_HTTP_HANDLER_MATCHER_HPP
 #define SRC_LIB_HTTP_HANDLER_MATCHER_HPP
+
 #include "utils/types/option.hpp"
-
-
 #include <map>
-
+#include <iterator>
 
 namespace http {
     template <typename T>
@@ -14,8 +13,24 @@ namespace http {
         explicit Matcher(const Map &map) : map_(map) {}
         ~Matcher() {}
 
-        Option<T> match(const std::string &key) const {
-
+        Option<T> match(const std::string &key) {
+            if (key.size() > 1 && key[key.size() - 1] == '/') {
+                return match(key.substr(0, key.size() - 1));
+            }
+            if (key.find("//") != std::string::npos) {
+                return match(key.substr(0, key.find("//") + 1) + key.substr(key.find("//") + 2));
+            }
+            typename Map::const_iterator it = map_.find(key);
+            if (it == map_.end()) {
+                if (key.find_last_of('/') != std::string::npos) {
+                    if (key.substr(0, key.find_last_of('/')).empty()) { // /root -> / になるのを防ぎたい
+                        return match("/");
+                    }
+                    return match(key.substr(0, key.find_last_of('/')));
+                }
+                return None;
+            }
+            return Some(it->second);
         }
 
     private:
