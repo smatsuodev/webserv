443d08670a666aa4d34d4a16ddcccd2703456538
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-01-05 10:23:09 +0900

    fix: exception on unwrap connection

--- src/lib/core/handler/accept_handler.cpp
+++ src/lib/core/handler/accept_handler.cpp
@@ -19,12 +19,17 @@ IEventHandler::InvokeResult AcceptHandler::invoke(const Context &ctx) {
 
     Connection *newConnection = result.unwrap();
     const Event eventToRegister = Event(newConnection->getFd(), Event::kRead);
+    /**
+     * 引数の Context には現時点で Connection が含まれていないので、新しい Context を作成する
+     * NOTE: ReadRequestHandler にそもそも Context が必要か?
+     */
+    const Context newCtx(Some(Ref<Connection>(*newConnection)), eventToRegister, ctx.getResolver());
 
     // caller が処理すべき action を返す
     std::vector<IAction *> actions;
     actions.push_back(new RegisterEventAction(eventToRegister));
     actions.push_back(new AddConnectionAction(newConnection));
-    actions.push_back(new RegisterEventHandlerAction(*newConnection, new ReadRequestHandler(ctx)));
+    actions.push_back(new RegisterEventHandlerAction(*newConnection, new ReadRequestHandler(newCtx)));
 
     return Ok(actions);
 }
