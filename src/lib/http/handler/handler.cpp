9d39a20d21eba21659b562316858e2d9c8734b44
shmorish <110565242+shmorish@users.noreply.github.com>
2025-01-06 12:25:59 +0900

    DELETEメソッドの処理を修正

--- src/lib/http/handler/handler.cpp
+++ src/lib/http/handler/handler.cpp
@@ -9,6 +9,23 @@
 http::IHandler::~IHandler() {}
 
 http::Response http::Handler::deleteMethod(const std::string &path) {
+    if (std::remove(path.c_str())) {
+        LOG_DEBUGF("failed to delete file: %s", path.c_str());
+        return ResponseBuilder().status(kStatusInternalServerError).build();
+    }
+    LOG_DEBUGF("file deleted: %s", path.c_str());
+    return ResponseBuilder().status(kStatusNoContent).build();
+}
+
+http::Response http::Handler::serve(const http::Request &req) {
+    LOG_DEBUGF("request target: %s", req.getRequestTarget().c_str());
+    const std::string path = req.getRequestTarget().substr(1);
+
+    if (path.empty()) {
+        LOG_DEBUGF("invalid request target: %s", req.getRequestTarget().c_str());
+        return ResponseBuilder().status(kStatusNotFound).build();
+    }
+
     struct stat buf = {};
     if (stat(path.c_str(), &buf) == -1) {
         if (errno == ENOENT) {
@@ -27,23 +44,6 @@ http::Response http::Handler::deleteMethod(const std::string &path) {
     if (!S_ISREG(buf.st_mode)) {
         LOG_DEBUGF("is not a regular file: %s", path.c_str());
         return ResponseBuilder().status(kStatusForbidden).build();
-    }
-
-    if (std::remove(path.c_str())) {
-        LOG_DEBUGF("failed to delete file: %s", path.c_str());
-        return ResponseBuilder().status(kStatusInternalServerError).build();
-    }
-    LOG_DEBUGF("file deleted: %s", path.c_str());
-    return ResponseBuilder().status(kStatusNoContent).build();
-}
-
-http::Response http::Handler::serve(const http::Request &req) {
-    LOG_DEBUGF("request target: %s", req.getRequestTarget().c_str());
-    const std::string path = req.getRequestTarget().substr(1);
-
-    if (path.empty()) {
-        LOG_DEBUGF("invalid request target: %s", req.getRequestTarget().c_str());
-        return ResponseBuilder().status(kStatusNotFound).build();
     }
 
     if (req.getMethod() == kMethodDelete) {
