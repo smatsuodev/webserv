1279237a670b07106cf8df5bb0ef5d47139a1a0f
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-01-15 20:16:32 +0900

    サンプルのルーティングの見直し

--- src/cmd/main.cpp
+++ src/cmd/main.cpp
@@ -8,15 +8,15 @@ using namespace config;
 // 仮の設定。本当は設定ファイルから読み込む
 Config loadConfig() {
     LocationContext location1(
-        "/", LocationContext::DocumentRootConfig("./"), {http::kMethodGet, http::kMethodPost, http::kMethodDelete}
+        "/", LocationContext::DocumentRootConfig("./", true), {http::kMethodGet, http::kMethodPost, http::kMethodDelete}
     );
-    // src/ は GET だけ許可
+    // src/ は GET だけ許可。autoindex も有効化しない
     LocationContext location2("/src", LocationContext::DocumentRootConfig("./"));
     ServerContext server1("localhost", 8080, {location1, location2});
 
-    ServerContext server2(
-        "0.0.0.0", 8081, {LocationContext("/", LocationContext::DocumentRootConfig("./example/html"))}
-    );
+    LocationContext location3("/", LocationContext::DocumentRootConfig("./example/html", true));
+    LocationContext location4("/other", LocationContext::DocumentRootConfig("./example/html", true, "home.html"));
+    ServerContext server2("0.0.0.0", 8081, {location3, location4});
 
     // 同じ 8081 ポートで別のサーバーを定義
     // 全部 example.com にリダイレクト
