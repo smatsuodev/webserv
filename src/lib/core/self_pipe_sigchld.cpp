ab47859f3aac442d68cc234a3bc917eed863d584
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-08-17 05:27:47 +0900

    helper

---
+++ src/lib/core/self_pipe_sigchld.cpp
@@ -0,0 +1,65 @@
+#include "self_pipe_sigchld.hpp"
+#include "utils/fd.hpp"
+#include <unistd.h>
+
+SelfPipeSigchld *SelfPipeSigchld::instance_ = NULL;
+
+SelfPipeSigchld::SelfPipeSigchld() : readFd_(-1), writeFd_(-1), prev_(SIG_DFL) {
+    int pipeFds[2];
+    if (pipe(pipeFds) == -1) {
+        return;
+    }
+
+    readFd_ = pipeFds[0];
+    writeFd_ = pipeFds[1];
+
+    utils::setNonBlocking(readFd_);
+    utils::setNonBlocking(writeFd_);
+    utils::setCloseOnExec(readFd_);
+    utils::setCloseOnExec(writeFd_);
+
+    // シングルトン的に、このインスタンスをハンドラから参照する
+    instance_ = this;
+
+    prev_ = signal(SIGCHLD, &SelfPipeSigchld::handler);
+    signal(SIGPIPE, SIG_IGN);
+}
+
+SelfPipeSigchld::~SelfPipeSigchld() {
+    signal(SIGCHLD, prev_);
+    if (readFd_ != -1) {
+        close(readFd_);
+    }
+    if (writeFd_ != -1) {
+        close(writeFd_);
+    }
+    instance_ = NULL;
+}
+
+void SelfPipeSigchld::registerWithEventNotifier(IEventNotifier *notifier) const {
+    if (readFd_ < 0) {
+        return;
+    }
+    notifier->registerEvent(Event(readFd_, Event::kRead));
+}
+
+// self-pipe を "空" にする (課題の制約上、沢山書き込まれていると空にはならない)
+void SelfPipeSigchld::drain() const {
+    if (readFd_ < 0) {
+        return;
+    }
+    char buf[256];
+    read(readFd_, buf, sizeof(buf));
+}
+
+int SelfPipeSigchld::getReadFd() const {
+    return readFd_;
+}
+
+// signal handler から pipe 経由で通知を送る
+void SelfPipeSigchld::handler(int) {
+    if (instance_ && instance_->writeFd_ >= 0) {
+        const char c = 1;
+        write(instance_->writeFd_, &c, 1);
+    }
+}
